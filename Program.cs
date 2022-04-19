c3797c361f3116a73b195a896f64b6f09ab5a5fc
kasyoki.justus <justuskasyoki57@gmail.com>
2022-04-19 12:19:57 +0300

    commit

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TremolZFP;
 
 namespace KHtest
 {
@@ -10,6 +11,15 @@ namespace KHtest
     {
         static void Main(string[] args)
         {
+
+            FP fP = new FP() { ServerAddress = "http://LocalHost:4444/" };
+            fP.ServerSetDeviceTcpSettings("62.8.75.229", 8000, "Password");
+          //  fP.ServerSetDeviceSerialPortSettings("COM/com", 115200);
+            fP.OpenReceipt((OptionReiptceFormat)'1', "Password");
+            fP.SellPLUfromExtDB("", OptionVATClass.VAT_Class_A, 1.29m, "", null, null, 8.9m, null, 34.2m);
+            fP.ReadLastAndTotalReceiptNum();
+            fP.CloseReceipt();
+            Console.WriteLine();
         }
     }
 }
