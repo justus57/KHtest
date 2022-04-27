96e687630e6889638055e960762aa593f5db8942
kasyoki.justus <justuskasyoki57@gmail.com>
2022-04-27 10:07:13 +0300

    commit

--- Program.cs
+++ Program.cs
@@ -13,13 +13,19 @@ namespace KHtest
         {
 
             FP fP = new FP() { ServerAddress = "http://LocalHost:4444/" };
+
             fP.ServerSetDeviceTcpSettings("62.8.75.229", 8000, "Password");
+
           //  fP.ServerSetDeviceSerialPortSettings("COM/com", 115200);
+
             fP.OpenReceipt((OptionReceiptFormat)'1', "115200");
 
             fP.SellPLUfromExtDB("Sample", OptionVATClass.VAT_Class_A, 1.29m, "23.0", null, null, 8.9m, null, 34.2m);
+
             fP.ReadLastAndTotalReceiptNum();
+
             fP.CloseReceipt();
+
             Console.WriteLine();
         }
     }
